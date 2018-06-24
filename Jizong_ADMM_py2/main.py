e908934c8953d9f04b104d1216770da6343f2279
root <pengjizong@sjtu.edu.cn>
2018-06-24 11:02:26 -0700

    adapted to python2

--- Jizong_ADMM_py2/main.py
+++ Jizong_ADMM_py2/main.py
@@ -1,4 +1,4 @@
-import sys,os
+import sys,os,copy
 from torch.utils.data import DataLoader
 from torchvision import transforms
 import medicalDataLoader
@@ -18,7 +18,7 @@ device = "cuda" if torch.cuda.is_available() and use_gpu else "cpu"
 
 batch_size = 4
 batch_size_val = 4
-num_workers = 4
+num_workers = 0
 lr = 0.001
 max_epoch = 100
 root_dir = '../ACDC-2D-All'
@@ -36,8 +36,24 @@ mask_transform = transforms.Compose([
 train_set = medicalDataLoader.MedicalImageDataset('train',root_dir,transform=transform,mask_transform=mask_transform,augment=True,equalize=False)
 train_loader = DataLoader(train_set,batch_size=batch_size,num_workers=num_workers,shuffle=True)
 
+## Here we have to split the fully annotated dataset and unannotated dataset
+split_ratio = 0.2
+random_index = np.random.permutation(len(train_set))
+labeled_dataset = copy.deepcopy(train_set)
+labeled_dataset.imgs = [train_set.imgs[x] for x in  random_index[:int(len(random_index)*split_ratio)]]
+unlabeled_dataset = copy.deepcopy(train_set)
+unlabeled_dataset.imgs = [train_set.imgs[x] for x in  random_index[int(len(random_index)*split_ratio):]]
+assert set(unlabeled_dataset.imgs)&set(labeled_dataset.imgs) ==set(), "there's intersection between labeled and unlabeled training set."
+labeled_dataLoader = DataLoader(labeled_dataset,batch_size=batch_size,num_workers=num_workers,shuffle=True)
+unlabeled_dataLoader = DataLoader(unlabeled_dataset,batch_size=batch_size,num_workers=num_workers,shuffle=True)
+## Here we terminate the split of labeled and unlabeled data
+
+## the validation set is for computing the dice loss.
 val_set = medicalDataLoader.MedicalImageDataset('val',root_dir,transform=transform,mask_transform=mask_transform,equalize=False)
 val_loader = DataLoader(val_set, batch_size=batch_size_val, num_workers=num_workers, shuffle=True)
+##
+##=====================================================================================================================#
+
 num_classes=2
 net = UNet(num_classes=num_classes).to(device)
 # net.load_state_dict(torch.load('U_net_2Class.pth'))
@@ -59,6 +75,7 @@ if __name__=="__main__":
                 print('learning rate:', param_group['lr'])
             print('save model:')
             torch.save(net.state_dict(), 'U_net_2Class.pth')
+
 
         for i, (img,mask,_,_) in tqdm(enumerate(train_loader)):
             img,mask=img.to(device),mask.to(device)
